619850096dbaf6e0d1ac9f41c1a9dde18fa722f8
Dang Khoa <dangkhoa@email.com>
2026-03-18 15:41:42 +0700

    Initial Commit

---
+++ QuanLy_PhongTroj/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuanLy_PhongTroj
+//
+//  Created by mac on 18/3/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
